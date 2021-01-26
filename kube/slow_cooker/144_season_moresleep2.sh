20ea01f2f48ca8951d7527fa27799db5f1088af1
Thomas Wang <thomas.wang@ericsson.com>
2021-01-26 17:06:12 +0900

    changed port

--- kube/slow_cooker/144_season_moresleep2.sh
+++ kube/slow_cooker/144_season_moresleep2.sh
@@ -2,7 +2,7 @@
 
 dou=1
 
-timeout -s SIGINT 1080 go run main.go -qps $it -concurrency 100 http://10.129.9.3:32062
+timeout -s SIGINT 1080 go run main.go -qps $it -concurrency 100 http://10.129.9.3:30426
 while [ $dou -le 10 ]
 SECONDS=0
 do
@@ -12,12 +12,12 @@ do
         let "it = $counter * 7"
         echo $it
         echo Put load $it load 60s
-        timeout -s SIGINT 40 go run main.go -qps $it -concurrency 100 http://10.129.9.3:32062
+        timeout -s SIGINT 40 go run main.go -qps $it -concurrency 100 http://10.129.9.3:30426
         # sleep 1
         ((counter++))
         
     done
-    timeout -s SIGINT 60 go run main.go -qps 70 -concurrency 100 http://10.129.9.3:32062
+    timeout -s SIGINT 60 go run main.go -qps 70 -concurrency 100 http://10.129.9.3:30426
 
     trap 'exit' INT
 
@@ -27,13 +27,13 @@ do
         let "it = $counter * 7"
         echo $it
         echo Put load $it load 60s
-        timeout -s SIGINT 40 go run main.go -qps $it -concurrency 100 http://10.129.9.3:32062
+        timeout -s SIGINT 40 go run main.go -qps $it -concurrency 100 http://10.129.9.3:30426
         # sleep 1
         ((counter--))
         
     done
     trap 'exit' INT
-    timeout -s SIGINT 1300 go run main.go -qps $it -concurrency 100 http://10.129.9.3:32062
+    timeout -s SIGINT 1300 go run main.go -qps $it -concurrency 100 http://10.129.9.3:30426
 
     elapsedseconds=$SECONDS
     echo $elapsedseconds
